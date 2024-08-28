a2f1b18d92abba75c3f003ef9e781a50558deea5
svladimirdev <svladimir.dev@gmail.com>
2024-08-28 21:44:47 +0300

    Valuation project

---
+++ lesson03/app.js
@@ -0,0 +1,6 @@
+let titleProject = 'Valuation';
+let screensValue = 'home custom page, blog page, catalog, autorization page, cart page';
+let screenPrice = 125;
+let percentage = 20;
+let fullPrice = 625;
+let responsive = true;
