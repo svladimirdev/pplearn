f00ce51e5665504421d3bad56c4ea50cb4f96694
svladimirdev <svladimir.dev@gmail.com>
2024-08-28 22:11:26 +0300

    lesson03

--- lesson03/lesson-03.js
+++ lesson03/lesson-03.js
@@ -1,12 +1,3 @@
-let projectValuation = {
-    titleProject : 'Valuation',
-    screensValue : 'home custom page, blog page, catalog, autorization page, cart page',
-    screenPrice : 125,
-    percentage : 20,
-    fullPrice : 625,
-    responsive : true,
-}
-
 let titleProject = 'Valuation';
 let screensValue = 'home custom page, blog page, catalog, autorization page, cart page';
 let screenPrice = 125;
